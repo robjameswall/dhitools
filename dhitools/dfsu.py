8522a484b3a0d9b25d589ba52d628b19d6b701a1
WALL Robert <robert.wall@des.qld.gov.au>
2018-06-27 11:45:12 +1000

    added detailed docstrings

--- dhitools/dfsu.py
+++ dhitools/dfsu.py
@@ -1,19 +1,22 @@
 """DHI MIKE21 dfsu functions
-"""
 
-# Author: Robert Wall
+Author: Robert Wall
+
+"""
 
 import numpy as np
 from dotenv import load_dotenv, find_dotenv
 import os
 import clr
 
+# Set path to MIKE SDK
 load_dotenv(find_dotenv())
 sdk_path = os.getenv('MIKE_SDK')
 dfs_dll = os.getenv('MIKE_DFS')
 clr.AddReference(os.path.join(sdk_path, dfs_dll))
 clr.AddReference('System')
 
+# Import .NET libraries
 import DHI.Generic.MikeZero.DFS as dfs
 import System
 from System import Array
@@ -23,18 +26,53 @@ import ctypes
 
 
 class Dfsu():
+    """
+    MIKE21 .dfsu
+
+    Parameters
+    ----------
+    filename : str
+        Path to .dfsu
+
+    Attributes
+    ----------
+    filename : str
+        Path to .dfsu
+    items : dict
+        List .dfsu items (ie. surface elevation, current speed), item index
+        to lookup in .dfsu, item units and counts of elements, nodes and
+        time steps.
+    projection : str
+        .dfsu spatial projection string in WKT format
+    ele_table : ndarray, shape (num_ele, 3)
+        Defines for each element the nodes that define the element.
+    node_table : ndarray, shape (num_nodes, n)
+        Defines for each node the element adjacent to this node. May contain
+        padded zeros
+    nodes : ndarray, shape (num_nodes, 3)
+        (x,y,z) coordinate for each node
+    elements : ndarray, shape (num_ele, 3)
+        (x,y,z) coordinate for each element
+
+    See Also
+    ----------
+    Many of these methods have been adapated from the DHI MATLAB Toolbox:
+        https://www.mikepoweredbydhi.com/download/mike-by-dhi-tools/
+        coastandseatools/dhi-matlab-toolbox
+    """
 
     def __init__(self, filename):
 
         self.read_dfsu(filename)
 
     def read_dfsu(self, filename):
+        """ Read in .dfsu file and read attributes """
         self.filename = filename
         dfsu_object = dfs.DfsFileFactory.DfsuFileOpen(filename)
         self.items = _dfsu_info(dfsu_object)
         self.projection = str(dfsu_object.Projection.WKTString)
         self.ele_table = _element_table(dfsu_object)
-        self.NtoE = _node_to_element_table(dfsu_object, self.ele_table)
+        self.node_table = _node_table(dfsu_object, self.ele_table)
         self.nodes = _node_coordinates(dfsu_object)
         self.elements = _element_coordinates(dfsu_object)
 
@@ -45,6 +83,35 @@ class Dfsu():
 
     def item_element_data(self, item_name, tstep_start=None, tstep_end=None,
                           element_list=None):
+        """
+        Get element data for specified item with option to specify range of
+        timesteps.
+
+        Parameters
+        ----------
+        item_name : str
+            Specified item to return element data. Item names are found in
+            the `Dfsu.items` attribute.
+        tstep_start : int or None, optional
+            Specify time step for element data. Timesteps begin from 0.
+            If `None`, returns data from 0 time step.
+        tstep_end : int or None, optional
+            Specify last time step for element data. Allows for range of time
+            steps to be returned, where `tstep_end` is included.Must be
+            positive int <= number of timesteps
+            If `None`, returns single time step specified by `tstep_start`
+            If `-1`, returns all time steps from `tstep_start`:end
+        element_list : list, optional
+            Provide list of elements. Element numbers are as seen by MIKE
+            programs and adjusted for Python indexing.
+
+        Returns
+        -------
+        ele_data : ndarray, shape (num_elements,[tstep_end-tstep_start])
+            Element data for specified item and time steps
+            `element_list` will change num_elements returned in `ele_data`
+
+        """
         dfsu_object = dfs.DfsFileFactory.DfsuFileOpen(self.filename)
         ele_data = _element_data(dfsu_object=dfsu_object, item_name=item_name,
                                  item_info=self.items, tstep_start=tstep_start,
@@ -54,10 +121,34 @@ class Dfsu():
         return ele_data
 
     def item_node_data(self, item_name, tstep_start=None, tstep_end=None):
+        """
+        Get node data for specified item with option to specify range of
+        timesteps.
+
+        Parameters
+        ----------
+        item_name : str
+            Specified item to return node data. Item names are found in
+            the `Dfsu.items` attribute.
+        tstep_start : int or None, optional
+            Specify time step for node data. Timesteps begin from 0.
+            If `None`, returns data from 0 time step.
+        tstep_end : int or None, optional
+            Specify last time step for node data. Allows for range of time
+            steps to be returned, where `tstep_end` is included.Must be
+            positive int <= number of timesteps
+            If `None`, returns single time step specified by `tstep_start`
+            If `-1`, returns all time steps from `tstep_start`:end
+
+        Returns
+        -------
+        node_data : ndarray, shape (num_elements,[tstep_end-tstep_start])
+            Node data for specified item and time steps
+        """
         dfsu_object = dfs.DfsFileFactory.DfsuFileOpen(self.filename)
         node_data = _node_data(dfsu_object=dfsu_object, item_name=item_name,
                                item_info=self.items, ele_cords=self.elements,
-                               node_cords=self.nodes, NtoE=self.NtoE,
+                               node_cords=self.nodes, node_table=self.node_table,
                                tstep_start=tstep_start, tstep_end=tstep_end)
         dfsu_object.Close()
 
@@ -65,6 +156,40 @@ class Dfsu():
 
     def max_item(self, item_name, tstep_start=None, tstep_end=None,
                  current_dir=False):
+        """
+        Calculate maximum element value for specified item over entire model or
+        within specific range of timesteps.
+
+        Parameters
+        ----------
+        item_name : str
+            Specified item to return element data. Item names are found in
+            the `Dfsu.items` attribute.
+        tstep_start : int or None, optional
+            Specify time step for data considered in determining maximum.
+            Timesteps begin from 0.
+            If `None`, returns data from 0 time step.
+        tstep_end : int or None, optional
+            Specify last time step for data considered in determining maximum
+            Must be positive int <= number of timesteps
+            If `None`, returns all time steps from `tstep_start`:end
+        current_dir : boolean
+            If True, returns corresponding current direction value occuring at
+            the maxmimum of specified `item_name`.
+
+        Returns
+        -------
+        If `current_dir` is False:
+        max_ele : ndarray, shape (num_elements,)
+            Maximum elements values for specified item
+
+        If `current_dir` is True
+        max_ele : ndarray, shape (num_elements,)
+            Maximum elements values for specified item
+        max_current_dir : ndarray, shape (num_elements,)
+            Current direction corresponding to `max_ele`
+
+        """
         dfsu_object = dfs.DfsFileFactory.DfsuFileOpen(self.filename)
         max_ele = _item_aggregate_stats(dfsu_object, item_name,
                                         self.items, tstep_start=tstep_start,
@@ -79,6 +204,41 @@ class Dfsu():
 
     def min_item(self, item_name, tstep_start=None, tstep_end=None,
                  current_dir=False):
+        """
+        Calculate minimum element value for specified item over entire model or
+        within specific range of timesteps.
+
+        Parameters
+        ----------
+        item_name : str
+            Specified item to return element data. Item names are found in
+            the `Dfsu.items` attribute.
+        tstep_start : int or None, optional
+            Specify time step for data considered in determining minimum.
+            Timesteps begin from 0.
+            If `None`, returns data from 0 time step.
+        tstep_end : int or None, optional
+            Specify last time step for data considered in determining minimum
+            Must be positive int <= number of timesteps
+            If `None`, returns all time steps from `tstep_start`:end
+        current_dir : boolean
+            If True, returns corresponding current direction value occuring at
+            the maxmimum of specified `item_name`.
+
+        Returns
+        -------
+        If `current_dir` is False:
+        min_ele : ndarray, shape (num_elements,)
+            Minimum elements values for specified item
+
+        If `current_dir` is True
+        min_ele : ndarray, shape (num_elements,)
+            Minimum elements values for specified item
+        max_current_dir : ndarray, shape (num_elements,)
+            Current direction corresponding to `min_ele`
+
+        """
+
         dfsu_object = dfs.DfsFileFactory.DfsuFileOpen(self.filename)
         min_ele = _item_aggregate_stats(dfsu_object, item_name,
                                         self.items, tstep_start=tstep_start,
@@ -87,13 +247,17 @@ class Dfsu():
         dfsu_object.Close()
 
         if current_dir:
-            return max_ele[0], max_ele[1]
+            return min_ele[0], min_ele[1]
         else:
-            return max_ele
+            return min_ele
 
 
 def _dfsu_info(dfsu_object):
+    """
+    Make a dictionary with .dfsu items and other attributes.
 
+    See class attributes
+    """
     itemnames = [[n.Name, n.Quantity.UnitAbbreviation] for n in dfsu_object.ItemInfo]
     items = {}
 
@@ -115,12 +279,13 @@ def _dfsu_info(dfsu_object):
     return items
 
 
-'''
+"""
 Underlying dfsu mesh node and element functions
-'''
+"""
 
 
 def _element_table(dfsu_object):
+    """ Defines for each element the nodes that define the element """
     ele_table_object = dfsu_object.ElementTable
     element_table = np.zeros(shape=(dfsu_object.NumberOfElements,3), dtype='int')
     for i, e in enumerate(ele_table_object):
@@ -128,33 +293,35 @@ def _element_table(dfsu_object):
     return element_table
 
 
-def _node_to_element_table(dfsu_object, ele_table):
+def _node_table(dfsu_object, ele_table):
+    """ Create node_table from ele_table """
     num_nodes = dfsu_object.NumberOfNodes
     num_elements = dfsu_object.NumberOfElements
 
-    # Set placeholders for constructing node-to-element-table (NtoE)
+    # Set placeholders for constructing node-to-element-table (node_table)
     e = np.arange(num_elements)
     u = np.ones(num_elements)
     I = np.concatenate((e, e, e))
     J = np.concatenate((ele_table[:,0],ele_table[:,1],ele_table[:,2]))
     K = np.concatenate((u*1, u*2, u*3))
 
-    # Construct NtoE
+    # Construct node_table
     count = np.zeros((num_nodes,1))
     for i in range(len(I)):
         count[J[i-1]-1] = count[J[i-1]-1]+1
     num_cols = int(count.max())
 
-    NtoE = np.zeros((num_nodes,num_cols), dtype='int')
+    node_table = np.zeros((num_nodes,num_cols), dtype='int')
     count = np.zeros((num_nodes,1))
     for i in range(len(I)):
         count[J[i-1]-1] = count[J[i-1]-1]+1
-        NtoE[J[i-1]-1, int(count[J[i-1]-1])-1] = I[i]
+        node_table[J[i-1]-1, int(count[J[i-1]-1])-1] = I[i]
 
-    return NtoE
+    return node_table
 
 
 def _node_coordinates(dfsu_object):
+    """ Read in node (x,y,z) """
     xn = np.array([point for point in dfsu_object.X])
     yn = np.array([point for point in dfsu_object.Y])
     zn = np.array([point for point in dfsu_object.Z])
@@ -164,6 +331,7 @@ def _node_coordinates(dfsu_object):
 
 
 def _element_coordinates(dfsu_object):
+    """ Read in element (x,y,z) """
     element_coordinates = np.zeros(shape=(dfsu_object.NumberOfElements, 3))
 
     # Convert nodes to .NET System double to input to method
@@ -184,22 +352,25 @@ def _element_coordinates(dfsu_object):
     return element_coordinates
 
 
-'''
+"""
 Read item node and element data
-'''
+"""
 
 
 def _single_to_ndarray(system_single):
-    '''
+    """
     Converts data read in with dotNet (i.e. reading dfsu through DHI .NET
     libraries) and efficiently converts System.Single[,] to numpy ndarray
 
-    Inputs:
-        - System.Single[,] : .NET Framwork single-precision float
+    Parameters
+    ----------
+    System.Single[,] : .NET Framwork single-precision float
 
-    Outputs:
-        - ndarray
-    '''
+    Returns
+    -------
+    ndarray : System.Single converted to ndarray
+
+    """
 
     src_hndl = GCHandle.Alloc(system_single, GCHandleType.Pinned)
 
@@ -217,7 +388,7 @@ def _single_to_ndarray(system_single):
 def _element_data(dfsu_object, item_name, item_info,
                   tstep_start=None, tstep_end=None,
                   element_list=None):
-
+    """ Read specified item_name element data """
     if element_list:
         # Subtract zero to match Python idx'ing
         element_list = [e-1 for e in element_list]
@@ -251,8 +422,9 @@ def _element_data(dfsu_object, item_name, item_info,
 
 
 def _node_data(dfsu_object, item_name, item_info,
-               ele_cords, node_cords, NtoE,
+               ele_cords, node_cords, node_table,
                tstep_start=None, tstep_end=None):
+    """ Read specified item_name node data """
 
     # Get item_name element data
     ele_data = _element_data(dfsu_object, item_name, item_info,
@@ -261,16 +433,49 @@ def _node_data(dfsu_object, item_name, item_info,
     # Get item_name node data
     node_data = np.zeros(shape=(len(node_cords), ele_data.shape[1]))
     for i in range(ele_data.shape[1]):
-        node_data[:,i] = _map_ele_to_node(NtoE, ele_cords, node_cords, ele_data[:,i])
+        node_data[:,i] = _map_ele_to_node(node_table, ele_cords, node_cords, ele_data[:,i])
 
     return node_data
 
 
-def _interp_node_z(nn,NtoE,xe,ye,ze,xn,yn):
-    '''
-    Interpolate zn node at node (xn,yn) from element (xe, ye, ze)
-    '''
-    nelmts = len(np.where(NtoE[nn,:] != 0)[0])
+def _interp_node_z(nn,node_table,xe,ye,ze,xn,yn):
+    """
+    Calculate value at node (xn,yn) from element center values (xe, ye, ze).
+
+    Attempts to use Psuedo Lapalce procedure by [Holmes, Connel 1989]. If this
+    fails, uses an inverse distance average.
+
+    Parameters
+    ----------
+    nn : int
+        Node number to solve node value.
+    node_table : ndarray, shape (num_nodes, n)
+        Defines for each node the element adjacent to this node. May contain
+        padded zeros
+    xe : ndarray, shape (num_elements, 1)
+        Element x vector
+    ye : ndarray, shape (num_elements, 1)
+        Element y vector
+    ze : ndarray, shape (num_elements, 1)
+        Element x vector
+    xn : ndarray, shape (num_nodes, 1)
+        Node x vector
+    yn : ndarray, shape (num_nodes, 1)
+        Node x vector
+
+    Returns
+    -------
+    weights : array, shape (n_components,)
+
+    See Also
+    -------
+    DHI MIKE MATLAB Toolbox; specifically `mzCalcNodeValuePL.m`
+
+    Holmes, D. G. and Connell, S. D. (1989), Solution of the
+        2D Navier-Stokes on unstructured adaptive grids, AIAA Pap.
+        89-1932 in Proc. AIAA 9th CFD Conference.
+    """
+    nelmts = len(np.where(node_table[nn,:] != 0)[0])
 
     if nelmts < 1:
         zn = np.nan
@@ -283,7 +488,7 @@ def _interp_node_z(nn,NtoE,xe,ye,ze,xn,yn):
     Ixy = 0
 
     for i in range(nelmts):
-        el_id = int(NtoE[nn,i]-1)
+        el_id = int(node_table[nn,i]-1)
         dx = xe[el_id] - xn[nn]
         dy = ye[el_id] - yn[nn]
         Rx = Rx + dx
@@ -303,7 +508,7 @@ def _interp_node_z(nn,NtoE,xe,ye,ze,xn,yn):
         zn = float(0)
 
         for i in range(nelmts):
-            el_id = int(NtoE[nn,i]-1)
+            el_id = int(node_table[nn,i]-1)
 
             omega = 1 + lamda_x*(xe[el_id]-xn[nn]) + lamda_y*(ye[el_id]-yn[nn])
             if omega < 0:
@@ -325,7 +530,7 @@ def _interp_node_z(nn,NtoE,xe,ye,ze,xn,yn):
         zn = 0
 
         for i in range(nelmts):
-            el_id = int(NtoE[nn,i]-1)
+            el_id = int(node_table[nn,i]-1)
 
             dx = xe[el_id] - xn[nn]
             dy = ye[el_id] - yn[nn]
@@ -342,11 +547,11 @@ def _interp_node_z(nn,NtoE,xe,ye,ze,xn,yn):
     return zn
 
 
-def _map_ele_to_node(NtoE, element_coordinates, node_coordinates,
+def _map_ele_to_node(node_table, element_coordinates, node_coordinates,
                      element_data):
-    '''
+    """
     Get node data relating to specific element
-    '''
+    """
     xn = node_coordinates[:,0]
     yn = node_coordinates[:,1]
     xe = element_coordinates[:,0]
@@ -355,21 +560,21 @@ def _map_ele_to_node(NtoE, element_coordinates, node_coordinates,
     zn = np.zeros(len(xn))
 
     for i in range(len(xn)):
-        zn[i] = _interp_node_z(i,NtoE,xe,ye,element_data,xn,yn)
+        zn[i] = _interp_node_z(i,node_table,xe,ye,element_data,xn,yn)
 
     return zn
 
 
-'''
+"""
 dfsu stats
-'''
+"""
 
 
 def _item_aggregate_stats(dfsu_object, item_name, item_info, return_max=True,
                           tstep_start=None, tstep_end=None, current_dir=False):
-    '''
+    """
     Return max or min for input item across entire model or specific time range
-    '''
+    """
     item_idx = item_info[item_name]['index'] + 1
     ele_data = np.zeros((item_info['num_elements']))
 
