245bbd92cec2fa8ca154afb8ebb446e5cbc693cb
Rob Wall <34440117+robjameswall@users.noreply.github.com>
2018-07-11 10:35:54 +1000

    include scipy as required package; v0.0.2

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='dhitools',
-    version='0.1.0',
+    version='0.0.2',
     description='Python tools for working with DHI MIKE21',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Rob Wall',
-    url='https://github.com/robjameswall',
+    author_email='rob.james.wall@gmail.com',
+    url='https://github.com/robjameswall/dhitools',
     license='LICENSE.txt',
     packages=['dhitools'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ],
     install_requires=[
         "numpy == 1.14.5",
         "pythonnet==2.3.0",
         "python-dotenv==0.8.2",
-        "PyCRS==0.1.3"
+        "PyCRS==0.1.3",
+        "scipy==1.1.0"
     ],
     data_files=['.env'],
     extras_requires={
-        'gis': ["scipy==1.1.0",
-                "GDAL==2.2.4",
+        'gis': ["GDAL==2.2.4",
                 "geopandas==0.3.0"]
     }
 )
