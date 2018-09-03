acf161e96e340987ff7859434e8c31eb7bb7eea2
Rob Wall <34440117+robjameswall@users.noreply.github.com>
2018-09-04 09:56:34 +1000

    version 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='dhitools',
-    version='0.0.4',
+    version='0.0.5',
     description='Python tools for working with DHI MIKE21',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
